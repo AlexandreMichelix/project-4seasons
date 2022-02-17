64dcaa4cd02978167239ded0424a56a112c6cbcb
alexandre <alexandre.michelix@dotic.fr>
2022-02-17 18:01:48 +0100

    finish btn season

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { isWithinInterval } from "date-fns";
 import { ThemeProvider, createUseStyles } from "react-jss";
 
 import Season from "./Season";
-import Modal from "./Modal";
-import Card from "./Card";
 import data from "./data.json";
 import ThemeButton from "./ThemeButton";
+
+import "./Card.css";
 
 const NextSeason = lazy(() => import("./NextSeason"));
 
@@ -18,14 +18,13 @@ const useStyles = createUseStyles({
     flexDirection: "column",
     alignItems: "center",
     fontFamily: "system-ui",
-    height: "100vh",
     backgroundColor: ({ theme }) => (theme === "light" ? "white" : "black"),
     color: ({ theme }) => (theme === "light" ? "black" : "white"),
   },
   header: {
     width: "100%",
-    display: "flex",
     justifyContent: "flex-end",
+    textAlign: "-webkit-right",
   },
   main: {
     display: "flex",
@@ -43,37 +42,76 @@ const useStyles = createUseStyles({
   },
 });
 
+const useStyles2 = createUseStyles({
+  content: { marginTop: 8, marginBottom: 8 },
+  actions: { marginTop: 8, marginBottom: 8 },
+  button: {
+    backgroundColor: "none",
+    borderRadius: "5px",
+    padding: "4px 8px",
+    fontWeight: 700,
+    cursor: "pointer",
+    height: "50px",
+    width: "150px",
+  },
+});
+
 function App() {
   const [theme, setTheme] = useState("light");
   const toggle = () => setTheme(theme === "dark" ? "light" : "dark");
   const classes = useStyles({ theme });
-  return (
-    <ThemeProvider theme={{ type: theme, toggle }}>
-      <div className={classes.root}>
-        <header className={classes.header}>
-          <ThemeButton />
-        </header>
-        <main className={classes.main}>
-          <div className={classes.content} data-testid="content">
-            <Season name={season[0]} />
+
+  const [open, setOpen] = useState(false);
+  const toggle2 = () => setOpen(!open);
+  const classes2 = useStyles2();
+
+  if (!open) {
+    return (
+      <ThemeProvider theme={{ type: theme, toggle }}>
+        <div className="card">
+          <div className={classes.root}>
+            <header className={classes.header}>
+              <ThemeButton />
+              <main className={classes.main}>
+                <div className={classes.content} data-testid="content">
+                  <Season name={season[0]} />
+                </div>
+                <button className={classes2.button} onClick={toggle2}>
+                  Next season ?
+                </button>
+              </main>
+              <div className="card-body">
+                <h5>{"Realized by Alexandre MICHELIX"}</h5>
+              </div>
+            </header>
           </div>
-          <div className={classes.actions} data-testid="actions">
-            <Modal label="Next season ?">
-              <Suspense fallback={<div>Loading...</div>}>
-                <NextSeason name={season[1].next} />
-              </Suspense>
-            </Modal>
+        </div>
+      </ThemeProvider>
+    );
+  } else {
+    return (
+      <ThemeProvider theme={{ type: theme, toggle }}>
+        <div className="card">
+          <div className={classes.root}>
+            <header className={classes.header}>
+              <ThemeButton />
+              <main className={classes.main}>
+                <Suspense id="1" fallback={<div>Loading...</div>}>
+                  <NextSeason name={season[1].next} />
+                </Suspense>
+                <button className={classes2.button} onClick={toggle2}>
+                  I'll be back !
+                </button>
+              </main>
+              <div className="card-body">
+                <h5>{"Realized by Alexandre MICHELIX"}</h5>
+              </div>
+            </header>
           </div>
-        </main>
-      </div>
-      <div className='cards'>
-         <Card
-          img='https://picsum.photos/id/54/400/300'
-          title='What I learned from my visit to The Upside Down'
-          author='Nancy Wheeler' />
-      </div>
-    </ThemeProvider>
-  );
+        </div>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
--- src/ThemeButton.style.js
+++ src/ThemeButton.style.js
@@ -2,6 +2,7 @@ import { createUseStyles } from "react-jss";
 
 const useStyles = createUseStyles((theme) => ({
   root: {
+    display: "flex",
     border: "none",
     padding: "4px 8px",
     fontWeight: 700,
